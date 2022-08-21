6ebe59bbe28e83fd4bfdec896dac154c942ba0fa
NHau66 <nguyenhau65466@gmail.com>
2022-08-22 00:42:04 +0700

    Xây dựng Logic phần Header #3

--- src/components/Layout/components/Search/index.js
+++ src/components/Layout/components/Search/index.js
@@ -10,6 +10,8 @@ import AccountItem from '~/components/AccountItem';
 
 import { SearchIcon } from '~/components/Icons';
 
+import { useDebounce } from '~/hooks';
+
 import classNames from 'classnames/bind';
 import styles from './Search.module.scss';
 
@@ -21,17 +23,19 @@ function Search() {
     const [showResult, setShowResult] = useState(true);
     const [loading, setLoading] = useState(false);
 
+    const debounced = useDebounce(searchValue, 500);
+
     const inputRef = useRef();
 
     useEffect(() => {
-        if (!searchValue.trim()) {
+        if (!debounced.trim()) {
             setSearchResult([]);
             return;
         }
 
         setLoading(true);
 
-        fetch(`https://tiktok.fullstack.edu.vn/api/users/search?q=${encodeURIComponent(searchValue)}&type=less`)
+        fetch(`https://tiktok.fullstack.edu.vn/api/users/search?q=${encodeURIComponent(debounced)}&type=less`)
             .then((res) => res.json())
             .then((res) => {
                 setSearchResult(res.data);
@@ -40,7 +44,7 @@ function Search() {
             .catch(() => {
                 setLoading(false);
             });
-    }, [searchValue]);
+    }, [debounced]);
 
     const handleClear = () => {
         setSearchValue('');

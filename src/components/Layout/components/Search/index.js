ec4a53f306f60be1dce9a69c8a4830a8dbf97428
NHau66 <nguyenhau65466@gmail.com>
2022-08-22 22:49:53 +0700

    Tìm hiểu và sử dụng thư viện Axios

--- src/components/Layout/components/Search/index.js
+++ src/components/Layout/components/Search/index.js
@@ -1,19 +1,16 @@
 import { useEffect, useState, useRef } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCircleXmark, faSpinner } from '@fortawesome/free-solid-svg-icons';
-
 import HeadlessTippy from '@tippyjs/react/headless';
-
-import { Wrapper as PopperWrapper } from '~/components/Popper';
-
-import AccountItem from '~/components/AccountItem';
-
-import { SearchIcon } from '~/components/Icons';
-
-import { useDebounce } from '~/hooks';
-
 import classNames from 'classnames/bind';
 import styles from './Search.module.scss';
+
+import * as searchServices from '~/apiServices/searchServices'
+import { Wrapper as PopperWrapper } from '~/components/Popper';
+import AccountItem from '~/components/AccountItem';
+import { SearchIcon } from '~/components/Icons';
+import { useDebounce } from '~/hooks';
+
 
 const cx = classNames.bind(styles);
 
@@ -33,17 +30,17 @@ function Search() {
             return;
         }
 
-        setLoading(true);
+        const fetchApi = async () => {
+            setLoading(true);
 
-        fetch(`https://tiktok.fullstack.edu.vn/api/users/search?q=${encodeURIComponent(debounced)}&type=less`)
-            .then((res) => res.json())
-            .then((res) => {
-                setSearchResult(res.data);
-                setLoading(false);
-            })
-            .catch(() => {
-                setLoading(false);
-            });
+            const result = await searchServices.search(debounced);
+            
+            setSearchResult(result);
+
+            setLoading(false)
+        }
+
+        fetchApi()
     }, [debounced]);
 
     const handleClear = () => {

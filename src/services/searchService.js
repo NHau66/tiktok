25bae119813af0ac1eb4c8c6890f4d442840413d
NHau66 <nguyenhau65466@gmail.com>
2022-08-23 19:52:05 +0700

    Tái cấu trúc và tối ưu code #1

--- src/services/searchService.js
+++ src/services/searchService.js
@@ -1,8 +1,8 @@
-import * as request from '~/utils/request';
+import * as httpRequest from '~/utils/httpRequest';
 
 export const search = async (q, type = 'less') => {
     try {
-        const res = await request.get(`users/search`, {
+        const res = await httpRequest.get(`users/search`, {
             params: {
                 q,
                 type,
@@ -11,6 +11,5 @@ export const search = async (q, type = 'less') => {
         return res.data;
     } catch (error) {
         console.log(error);
-        
     }
 };
